4d6419e012ded59818bf51b419711d700acd5c0c
ElijahUkeme <72954785+ElijahUkeme@users.noreply.github.com>
2022-08-13 19:02:02 -0700

    Delete MovieApp directory

--- MovieApp/app/src/main/java/com/elijah/ukeme/firebase/signup/movieapp/adapters/OnMovieListener.java
+++
@@ -1,7 +0,0 @@
-package com.elijah.ukeme.firebase.signup.movieapp.adapters;
-
-public interface OnMovieListener {
-
-    void onMovieClick(int position);
-    void onCategoryClick(String category);
-}
